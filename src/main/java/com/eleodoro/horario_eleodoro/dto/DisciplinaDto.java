38dd16353aa0a64c2562ae10ff68970b7025f73e
Zecapaugordinho69 <joas.martins@escola.pr.gov.br>
2024-09-11 09:20:00 -0300

    aaaa

--- src/main/java/com/eleodoro/horario_eleodoro/dto/DisciplinaDto.java
+++ src/main/java/com/eleodoro/horario_eleodoro/dto/DisciplinaDto.java
@@ -1,31 +1,43 @@
 package com.eleodoro.horario_eleodoro.dto;
 
+import com.eleodoro.horario_eleodoro.modelo.Disciplina;
 import com.eleodoro.horario_eleodoro.modelo.Professor;
 
-public class DiciplinaDto {
+public class DisciplinaDto {
  
     private String nome;
     private int cargaHoraria;
     private Professor professor;
-    public DiciplinaDto() {
+    
+    public DisciplinaDto() {
     }
+
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public int getCargaHoraria() {
         return cargaHoraria;
     }
+
     public void setCargaHoraria(int cargaHoraria) {
         this.cargaHoraria = cargaHoraria;
     }
+
     public Professor getProfessor() {
         return professor;
     }
+
     public void setProfessor(Professor professor) {
         this.professor = professor;
+    }
+    
+    public Disciplina novoDisciplina() {
+        return new Disciplina(nome, cargaHoraria, professor);
     }
 
     
